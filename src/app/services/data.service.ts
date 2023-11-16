a7aee5225a5778383254ab9d53d4eb33a0a77863
AnnaP <annapatarino@gmail.com>
2023-11-16 18:07:22 +0100

    added new books data

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 
 export interface Message {
-  fromName: string;
-  subject: string;
-  date: string;
+  title: string;
+  author: string;
+  genre: string;
+  publicationYear: number;
   id: number;
   read: boolean;
 }
@@ -14,61 +15,32 @@ export interface Message {
 export class DataService {
   public messages: Message[] = [
     {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
-      date: '9:32 AM',
-      id: 0,
-      read: false
-    },
-    {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
-      date: '6:12 AM',
+      title: 'The Sword of Destiny',
+      author: 'Andrzej Sapkowski',
+      genre: 'Fantasy',
+      publicationYear: 1992,
       id: 1,
-      read: false
+      read: true
     },
+    
     {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
+      title: 'The Fellowship of the Ring',
+      author: 'J.R.R. Tolkien',
+      genre: 'Fantasy',
+      publicationYear: 1954,
       id: 2,
       read: false
     },
+    
     {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
+      title: 'A Game of Thrones',
+      author: 'George R.R. Martin',
+      genre: 'Fantasy',
+      publicationYear: 1996,
       id: 3,
-      read: false
-    },
-    {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
-      id: 4,
-      read: false
-    },
-    {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
-      id: 5,
-      read: false
-    },
-    {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
-      id: 6,
-      read: false
-    },
-    {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
-      id: 7,
-      read: false
+      read: true
     }
+    
   ];
 
   constructor() { }
